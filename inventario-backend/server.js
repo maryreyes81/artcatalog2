1b92124a1911e45e4090462fdf85380a297f2d6f
Mary Reyes <maryreyes81@hotmail.com>
2026-03-09 09:53:28 -0600

    updates backend

--- inventario-backend/server.js
+++ inventario-backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const itemsRoutes = require("./routes/items.routes");
+const itemsRoutes = require("./routes/items");
 
 const app = express();
 
